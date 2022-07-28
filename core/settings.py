fc8beba358b71badd4d1ad89fa7e750d2674b374
Web Development <deve.publicmail@gmail.com>
2022-07-28 22:08:14 +0100

    exposed port

--- core/settings.py
+++ core/settings.py
@@ -106,7 +106,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_FORMS = {
     'signup': 'reward.forms.MyCustomSignupForm',
     'login': 'reward.forms.MyCustomLoginForm',
